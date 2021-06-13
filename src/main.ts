7f82dba2fab3d9211c427a94ee4bba0bba10cf53
Anderson Santos <anderson.santos@tuta.io>
2021-06-13 20:22:32 -0300

    feat: heroku config

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,15 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './transform.interceptor';
+import { Logger} from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('main.ts');
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('/api')
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
+  logger.log(`Application listen port ${process.env.PORT}`)
 }
 bootstrap();
